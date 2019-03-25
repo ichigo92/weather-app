5622ece28084be6fb99392f6ff29abf4bf31f796
Abdul Fatir <a.fatir@yahoo.com>
2019-03-25 06:56:24 +0000

    Updated the api code

--- app.py
+++ app.py
@@ -7,15 +7,6 @@ session = cluster.connect()
 
 app = Flask(__name__)
 
-cities = [
-	{'id': 1, 'name' : 'London', 'original': 'London'},
-	{'id': 2, 'name' : 'Islamabad', 'original' : 'Islamabad'},
-	{'id': 3, 'name' : 'Tehran', 'original' : 'Tehran'},
-	{'id': 4, 'name' : 'Madrid', 'original' : 'Madrid'},
-	{'id': 5, 'name' : 'Tokyo', 'original' : 'Tokyo'},
-	{'id': 6, 'name' : 'Mexico City', 'original' : 'Mexico City'},
-]
-
 API_KEY = '5cbb13c2517ad300c52fa1806fd7d94f'
 
 base_url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units={units}&APPID={API_KEY}'
@@ -118,10 +109,13 @@ def create_city():
 	
 	weather_url = base_url.format(city = request.form['city'], units = 'metric', API_KEY = API_KEY)
 	resp = requests.post(weather_url, data = {'city': request.form['city']} ).json()
-	last_id = cities[-1]['id'];
+	
+	last_id = session.execute("""SELECT COUNT(*) FROM weather.city""")
+
+	#last_id = cities[-1]['id'];
 	last_id += 1
 	print(last_id)
-	session.execute("""INSERT INTO weather.city(ID,Name,Original,Temperature,Description,Icon) VALUES({id},'{name}','{original}',{temperature},'{description}','{icon}')""".format(id=last_id, name=request.form['city'], original=request.form['city'], temperature=resp['main']['temp'], description=resp['weather'][0]['description'],icon=resp['weather'][0]['icon']))
+	session.execute("""INSERT INTO weather.city(id,name,original,temperature,description,icon) VALUES({id},'{name}','{original}',{temperature},'{description}','{icon}')""".format(id=last_id, name=request.form['city'], original=request.form['city'], temperature=resp['main']['temp'], description=resp['weather'][0]['description'],icon=resp['weather'][0]['icon']))
 	#cities.append({'id': last_id, 'name':request.form['city'], 'original':request.form['city']})
 	
 	return jsonify({'message': 'created new city with weather'})
@@ -132,14 +126,8 @@ def update_city(id):
 	if not request.json or not 'city' in request.json:
 		return jsonify({'Error': 'Record does not exist'}), 404
 
-	#session.execute("""IUPDATE weather.city SET Description='{description}' WHERE name='{name}')""".format(name=city, description=city))
-	for x in cities:
-		if x['id'] == id:
+	session.execute("""UPDATE weather.city SET name='{updated_name}' WHERE id='{id}')""".format(name=request.json['city'], id=id))
 
-			x['name'] = request.json['city']
-
-			break
-	get_weather_for_all()
 
 	return jsonify({'message':'updated: /weather/{}'.format(id)})
 
@@ -149,12 +137,7 @@ def delete_city(city):
 	if not city:
 		return jsonify({'Error': 'The city name is needed to delete'})
 	
-	#session.execute("""DELETE FROM weather.city WHERE name='{}'""".format(city))
-
-	for x in cities:
-		if x['name'] == city:
-			cities.remove(x)
-			break
+	session.execute("""DELETE FROM weather.city WHERE name='{}'""".format(city))
 
 	return jsonify({'message': 'deleted: /weather/{}'.format(city)}) 
 
@@ -163,14 +146,7 @@ def delete_city_by_id(id):
 	if not id:
 		return jsonify({'Error': 'The id is needed to delete'})
 	
-	#session.execute("""DELETE FROM weather.city WHERE name='{}'""".format(city))
-
-	for x in cities:
-		if x['id'] == id:
-			print(cities)
-			cities.remove(x)
-			print(cities)
-			break
+	session.execute("""DELETE FROM weather.city WHERE id='{}'""".format(id))
 	
 	return jsonify({'message': 'deleted: /weather/{}'.format(id)}) 
 
